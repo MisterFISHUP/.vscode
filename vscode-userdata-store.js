0daff20cb60cabbfba1fc6bdd2884129e262aadc
FISH UP <68397336+MisterFISHUP@users.noreply.github.com>
2022-07-29 04:02:04 +0200

    Update vscode-userdata-store.js

--- vscode-userdata-store.js
+++ vscode-userdata-store.js
@@ -1,16 +1,12 @@
-const githubUser = "MisterFISHUP";
-const githubRepo = ".vscode";
-const entries = [
-  { name: "/User/settings.json", filePath: "vim-keybindings.json" },
-  { name: "/User/keybindings.json", filePath: "keybindings.json" },
-];
-
-const dbName = "vscode-web-db";
-const dbVersion = 2;
-const tableName = "vscode-userdata-store";
-
 async function fetchData() {
+  const githubUser = "MisterFISHUP";
+  const githubRepo = ".vscode";
   const fetchBaseUrl = `https://api.github.com/repos/${githubUser}/${githubRepo}/contents/`;
+  const entries = [
+    { name: "/User/settings.json", filePath: "vim-keybindings.json" },
+    { name: "/User/keybindings.json", filePath: "keybindings.json" },
+  ];
+
   const data = [];
 
   for (const entry of entries) {
@@ -49,6 +45,10 @@ async function updateDB() {
   if (!indexedDB) {
     console.log("IndexedDB could not be found in this browser.");
   }
+
+  const dbName = "vscode-web-db";
+  const dbVersion = 2;
+  const tableName = "vscode-userdata-store";
 
   const request = indexedDB.open(dbName, dbVersion);
 
